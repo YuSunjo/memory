9896c9861afa44abbe698d07b1b44faec60f0659
YuSunjo <tnswh2023@gmail.com>
2025-06-04 19:07:45 +0900

    feat(entity): add auditing fields and enable JPA auditing

--- memory-domain/src/main/java/com/memory/domain/BaseTimeEntity.java
+++ memory-domain/src/main/java/com/memory/domain/BaseTimeEntity.java
@@ -1,8 +1,11 @@
 package com.memory.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -12,8 +15,11 @@ import java.time.LocalDateTime;
 @Getter
 public class BaseTimeEntity {
 
+    @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createDate;
 
+    @LastModifiedDate
     private LocalDateTime updateDate;
 
     private LocalDateTime deleteDate;

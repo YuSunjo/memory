8ad355ac159124c0bf9509a847e5f49e44169279
YuSunjo <tnswh2023@gmail.com>
2025-06-30 19:23:12 +0900

    feat(security): enable CORS with custom configuration

--- memory-common/src/main/java/com/memory/config/security/SecurityConfig.java
+++ memory-common/src/main/java/com/memory/config/security/SecurityConfig.java
@@ -9,6 +9,12 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -27,9 +33,23 @@ public class SecurityConfig {
     }
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.cors(
-                        AbstractHttpConfigurer::disable // CORS 설정 비활성화
+                        cors -> cors.configurationSource(corsConfigurationSource())
                 )
                 .csrf(
                         AbstractHttpConfigurer::disable // CSRF 보호 비활성화

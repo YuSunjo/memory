413718d1f322253311ac3a974ed00eeee7520362
YuSunjo <tnswh2023@gmail.com>
2025-07-14 10:16:51 +0900

    chore(deploy, scripts): Add delay when deploying

--- memory-infra/docker/prod/ec2/scripts/start_application.sh
+++ memory-infra/docker/prod/ec2/scripts/start_application.sh
@@ -4,9 +4,7 @@ aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS
 
 cd /home/ec2-user/my-memory
 
-# 기존 컨테이너와 이미지 제거
-docker-compose down
-docker rmi 228749872437.dkr.ecr.ap-northeast-2.amazonaws.com/my_memory/memory:latest
+
 
 # 새 이미지 가져오기 및 실행
 docker-compose pull
--- memory-infra/docker/prod/ec2/scripts/stop_application.sh
+++ memory-infra/docker/prod/ec2/scripts/stop_application.sh
@@ -3,3 +3,8 @@
 # 실행 중인 도커 컨테이너 중지
 cd /home/ec2-user/my-memory
 docker-compose down || true
+
+sleep 10
+
+# 기존 컨테이너와 이미지 제거
+docker rmi 228749872437.dkr.ecr.ap-northeast-2.amazonaws.com/my_memory/memory:latest || true

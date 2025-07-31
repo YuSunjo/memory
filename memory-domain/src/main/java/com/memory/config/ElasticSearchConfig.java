d5a5419609155d73f2c6939ee7084c91da64c6a8
YuSunjo <tnswh2023@gmail.com>
2025-07-31 11:00:47 +0900

    chore(elasticsearch): - add basic authentication support - add kibana config

--- memory-domain/src/main/java/com/memory/config/ElasticSearchConfig.java
+++ memory-domain/src/main/java/com/memory/config/ElasticSearchConfig.java
@@ -13,10 +13,21 @@ public class ElasticSearchConfig extends ElasticsearchConfiguration {
     @Value("${spring.elasticsearch.uris:http://localhost:9200}")
     private String elasticsearchUri;
 
+    @Value("${spring.elasticsearch.username:}")
+    private String username;
+
+    @Value("${spring.elasticsearch.password:}")
+    private String password;
+
     @Override
     public ClientConfiguration clientConfiguration() {
-        return ClientConfiguration.builder()
-                .connectedTo(elasticsearchUri.replace("http://", ""))
-                .build();
+        ClientConfiguration.MaybeSecureClientConfigurationBuilder builder = ClientConfiguration.builder()
+                .connectedTo(elasticsearchUri.replace("http://", ""));
+
+        if (!username.isEmpty() && !password.isEmpty()) {
+            builder.withBasicAuth(username, password);
+        }
+
+        return builder.build();
     }
 }
